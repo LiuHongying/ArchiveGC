8117f458bb9e1679ce1748af11a2edaac873bd38
includeBD <1658209874@qq.com>
2020-11-23 16:22:01 +0800

    组件

---
+++ archive-ui/src/components/SubmitFolder.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <el-button type="primary" v-if="roleJudgement" @click="submitWhouse()">提交入库</el-button>
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+  name: "ArchieveStorage",
+  data() {
+    return {};
+  },
+  props: {
+    roleJudgement: { type: Boolean, default: true },
+    selectRowData: { type: Array, default: null },
+    transDataGrid: {
+      type: Object,
+      default: function () {
+        return {};
+      },
+    },
+  },
+  mounted() {},
+  methods: {
+    submitWhouse() {
+      let _self = this;
+      var m = [];
+      let tab = _self.selectRowData;
+
+      if (_self.selectRowData.length == 0) {
+        _self.$message({
+          showClose: true,
+          message: _self.$t("message.pleaseSelectDC"),
+          duration: 2000,
+          type: "warning",
+        });
+        return;
+      }
+
+      var i;
+      for (i = 0; i < tab.length; i++) {
+        m.push(tab[i]["ID"]);
+      }
+
+      axios
+        .post("/dc/archiveStorage", JSON.stringify(m), {
+          headers: {
+            "Content-Type": "application/json;charset=UTF-8",
+          },
+        })
+        .then(function (response) {
+          if (response.data.code == 1) {
+            _self.$message({
+              showClose: true,
+              message: _self.$t("message.operationSuccess"),
+              duration: 2000,
+              type: "success",
+            });
+            _self.$refs.transDataGrid.loadGridInfo();
+            _self.$refs.transDataGrid.loadGridData();
+          } else {
+            _self.$message({
+              showClose: true,
+              message: _self.$t("message.operationFaild"),
+              duration: 5000,
+              type: "error",
+            });
+          }
+        })
+        .catch(function (error) {
+          _self.$message({
+            showClose: true,
+            message: _self.$t("message.operationFaild"),
+            duration: 5000,
+            type: "error",
+          });
+          console.log(error);
+        });
+    },
+  },
+  components: {},
+};
+</script>
+<style scoped>
+</style>
