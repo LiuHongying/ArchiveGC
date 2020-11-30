d298d4131f8746a3dd73d31db9d45e658b536d68
tr1314qq <tr1314qq@163.com>
2020-11-30 15:15:56 +0800

    Update FolderClassification.vue

--- archive-ui/src/views/dc/FolderClassification.vue
+++ archive-ui/src/views/dc/FolderClassification.vue
@@ -405,6 +405,9 @@
           <el-form-item>
             <ArchieveStorage :roleJudgement="true"  :selectRowData="selectedItems"></ArchieveStorage>
           </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click.native="exportData">{{$t("application.ExportExcel")}}</el-button>
+          </el-form-item>
         </el-form>  
       </el-col>
     </el-row>
@@ -436,13 +439,8 @@
           </el-container>
         </template>
         <template slot="paneR">
-          <split-pane
-            v-on:resize="onSplitResize"
-            :min-percent="20"
-            :default-percent="topPercent"
-            split="horizontal"
-          >
-            <template slot="paneL">
+          
+            <!-- <template slot="paneL"> -->
               <el-row>
                 <el-table
                   :height="tableHeight"
@@ -569,7 +567,7 @@
                   :total="itemCount"
                 ></el-pagination>
               </el-row>
-            </template>
+            <!-- </template> -->
             <template slot="paneR" v-if="tempShow">
               <el-tabs v-model="tabs.activeNum">
                 <el-tab-pane label="案卷" name="relevantFile">
@@ -583,7 +581,7 @@
                 <el-tab-pane label="文件" name="initFile"> </el-tab-pane>
               </el-tabs>
             </template>
-          </split-pane>
+          
         </template>
       </split-pane>
     </div>
@@ -601,6 +599,7 @@ import FolderAcl from "@/components/controls/FolderAcl";
 import StartWorkflow from "@/views/workflow/StartWorkflow";
 import StartupComponent from "@/views/workflow/StartupComponent.vue";
 import ArchieveStorage from "@/components/SubmitFolder.vue";
+import ExcelUtil from "@/utils/excel.js";
 import "url-search-params-polyfill";
 
 export default {
@@ -617,6 +616,7 @@ export default {
     StartWorkflow: StartWorkflow,
     StartupComponent: StartupComponent,
     ArchieveStorage: ArchieveStorage,
+    ExcelUtil: ExcelUtil,
   },
   data() {
     return {
@@ -1453,6 +1453,21 @@ export default {
         }
       });
       this.columnsInfo.dialogFormVisible = false;
+    },
+
+    exportData() {
+      let _self = this;
+      let params = {
+        URL: "/file/exportFolderPath",
+        gridName: _self.currentFolder.gridView,
+        folderId: _self.currentFolder.id,
+        orderBy: "MODIFIED_DATE desc",
+        pageSize: _self.pageSize,
+        pageIndex: (_self.currentPage - 1) * _self.pageSize,
+        lang: "zh-cn",
+      };
+      console.log(params);
+      ExcelUtil.export4Cnpe(params);
     },
   },
 };
