cd49c392f9b1f53bb5c3c581d990942acea3da91
tr1314qq <tr1314qq@163.com>
2020-11-30 16:44:34 +0800

    合并

--- archive-ui/src/views/workflow/BorrowFile.vue
+++ archive-ui/src/views/workflow/BorrowFile.vue
@@ -41,7 +41,7 @@
           v-bind:tableHeight="tableHeight"
           v-bind:isshowOption="true"
           v-bind:isshowSelection="true"
-          condition="IS_RELEASED=1 AND IS_CHILD=0 AND IS_CURRENT=1"
+          :condition="searchFileCondition"
           gridViewName="BorrowSequenceGrid"
           :optionWidth="1"
           :isShowMoreOption="false"
@@ -203,7 +203,7 @@ export default {
     // },
     beforeAddFile() {
       let _self=this;
-      this.getEcmcfgActive(this.workflowObj.ID,"start",function(ecmCfgActivity){
+      this.getEcmcfgActive(_self.workflowObj.ID,"start",function(ecmCfgActivity){
         _self.searchFileCondition=ecmCfgActivity.formCondition;
         _self.propertyVisible=true;
       });
