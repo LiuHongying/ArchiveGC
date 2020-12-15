9c1fee1e9738d45c3fcfd67cd31b7002e2834d02
includeBD <1658209874@qq.com>
2020-12-15 14:54:38 +0800

    首页点击事件

--- archive-ui/src/views/workflow/BorrowStartUp.vue
+++ archive-ui/src/views/workflow/BorrowStartUp.vue
@@ -315,8 +315,9 @@
                     });
                      })
                 },
-                closePage(pv){
-                    this.$emit("close");
+                closePage(){
+                    let flag = false;
+                    this.$emit("closedialog", flag);
                 },
                 // 保存文档
                 saveItem()
