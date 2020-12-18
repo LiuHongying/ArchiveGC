9c5486a6e68442e12ac1b41595e5b6c18507e92f
liuxiuxiu123 <liucegulu@163.com>
2020-12-18 16:29:35 +0800

    新增3d模型查看

---
+++ archive-ui/src/views/dc/ThreeDsViewer copy.vue
@@ -0,0 +1,71 @@
+<template>
+  <iframe key="#1" :src="objUrl" frameborder="0" width="100%" :height="itemHeight" ></iframe>
+</template>
+
+<script type="text/javascript">
+import { ModelObj } from 'vue-3d-model'
+export default {
+  name: "ThreeDsViewer",
+  components: {
+            ModelObj
+        },
+  data() {
+    return {
+		itemHeight: window.innerHeight - 50,
+		objUrl:""
+    };
+  },
+  computed: {},
+  props: {
+    id:{type:String},
+    format:{type:String}
+  },
+  created() {
+	if(this.id==null && this.$route.query.id){
+      this.id = this.$route.query.id;
+    }
+    if(this.format==null && this.$route.query.format){
+      this.format = this.$route.query.format;
+    }
+    this.loadUrl();
+    this.writeAudit(this.id)
+  },
+  methods: {
+	  loadUrl() {
+      let _self = this;
+      let getfileUrl =  _self.axios.defaults.baseURL+"/dc/getContent?id="+_self.id+"&token="+sessionStorage.getItem('access-token')+"&format=obj";
+      _self.objUrl = "./static/threeviewer/threeviewer.html?file="+encodeURIComponent(getfileUrl)
+	},
+	writeAudit(docId){
+      var m = new Map();
+      m.set("docId",docId)
+      m.set("actionName","ecm_read")
+      m.set("appName","portal")
+      axios
+        .post("/audit/addAudit", JSON.stringify(m))
+        .then(function(response){
+          
+        })
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
