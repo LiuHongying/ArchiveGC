1efeeffde58300274d989468393851a81330a6d3
breezerong <nstorm@163.com>
2020-12-22 11:40:45 +0800

    清除其他项目内容

--- archive-ui/src/views/docexchange/IEDManagement/IEDLayout/index.js
+++
@@ -1,2 +0,0 @@
-export {default as IEDAll} from './IEDButton'
-export {default as IEDTrack} from './TrackST'
--- archive-ui/src/views/docexchange/SysManagement/LogLayout/Index.js
+++
@@ -1,3 +0,0 @@
-export {default as TCSyncGrid} from './TCSync'
-export {default as TCPushGrid} from './TCPush'
-export {default as NetSyncGrid} from './NetSync'
