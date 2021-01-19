bff55118d9edda7fc325c3c20fb878d728705723
breezerong <nstorm@163.com>
2021-01-19 08:15:11 +0800

    导出Excel更新

--- archive-ui/src/views/record/ArchiveArrange.vue
+++ archive-ui/src/views/record/ArchiveArrange.vue
@@ -874,6 +874,7 @@ export default {
         gridName: _self.$refs.mainDataGrid.gridViewName,
         folderId: _self.currentFolder.id,
         orderBy: "MODIFIED_DATE desc",
+        condition: "IS_HIDDEN=0",
         pageSize: _self.pageSize*10,
         pageIndex: _self.currentPage - 1,
         lang: "zh-cn",
