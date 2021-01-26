cf275929c06ed691abebe5acbc5e07ea770c496b
includeBD <1658209874@qq.com>
2021-01-26 18:13:41 +0800

    报表1-7

--- archive-ui/src/views/record/RecordReport.vue
+++ archive-ui/src/views/record/RecordReport.vue
@@ -16,7 +16,7 @@
         >
           <el-tree
             :props="defaultProps"
-            :data="dataList"
+            :data="treeData"
             node-key="id"
             style="width: 100%"
             default-expand-all
@@ -25,55 +25,61 @@
           ></el-tree>
         </el-container>
       </template>
-      <template slot="paneR"> </template>
+      <template slot="paneR">
+        <component :is="reportCompent"> </component>
+      </template>
     </split-pane>
   </div>
 </template>
 <script>
+import DataLayout from "@/components/ecm-data-layout";
+import { workFlowMonth, avWorkFlow, srWorkFlow, otherFileFlow, designWorkFlow, commercialWorkFlow } from "./report/";
 export default {
+  name: "RecordReport",
   data() {
     return {
       // 本地存储高度名称
-      topStorageName: "ArchiveHandOverTopHeight",
-      // 非split pan 控制区域高度
-      startHeight: 140,
-      // 顶部百分比*100
-      topPercent: 65,
-      // 顶部除列表高度
-      topbarHeight: 35,
-      // 底部除列表高度
-      bottomHeight: 80,
+      leftStorageName: "PreArchiveftHeight",
+
+      asideHeight: window.innerHeight - 100,
+      treeHeight: window.innerHeight - 120,
+      asideWidth: "100%",
 
       treeData: [
         {
           id: "1-0",
           label: "工作计量统计",
-          component: "",
+          component: "workFlowMonth",
           children: [
             {
               id: "1-1",
               label: "设计文件工作量统计",
-              component: "",
+              component: "designWorkFlow",
+              children: "",
             },
             {
               id: "1-2",
               label: "商务文件工作量统计",
-              component: "",
+              component: "commercialWorkFlow",
+              children: "",
             },
             {
               id: "1-3",
               label: "声像文件工作量统计",
-              component: "",
+              component: "avWorkFlow",
+              children: "",
             },
             {
               id: "1-4",
               label: "科研文件工作量统计",
-              component: "",
+              component: "srWorkFlow",
+              children: "",
             },
             {
               id: "1-5",
               label: "其他类型文件工作量统计",
-              component: "",
+              component: "otherFileFlow",
+              children: "",
             },
           ],
         },
@@ -81,21 +87,25 @@ export default {
           id: "2-0",
           label: "档案接收",
           component: "",
+          children: "",
         },
         {
           id: "3-0",
           label: "档案升版",
           component: "",
+          children: "",
         },
         {
           id: "4-0",
           label: "档案作废",
           component: "",
+          children: "",
         },
         {
           id: "5-0",
           label: "非密档案利用情况",
           component: "",
+          children: "",
         },
       ],
 
@@ -103,16 +113,33 @@ export default {
         children: "children",
         label: "label",
       },
+
+      reportCompent: "",
     };
+  },
+  components: {
+    DataLayout: DataLayout,
+    workFlowMonth: workFlowMonth,
+    avWorkFlow: avWorkFlow,
+    srWorkFlow: srWorkFlow,
+    otherFileFlow: otherFileFlow,
+    designWorkFlow: designWorkFlow,
+    commercialWorkFlow: commercialWorkFlow,
+  },
+
+  created(){
+    this.leftPercent = this.getStorageNumber(this.leftStorageName, 20);
   },
 
   methods: {
-    onSplitResize(topPercent) {
-      // 顶部百分比*100
-      this.topPercent = topPercent;
-      this.setStorageNumber(this.topStorageName, topPercent);
+    resize(leftPercent) {
+      // 左边百分比*100
+      this.leftPercent = leftPercent;
+      this.setStorageNumber(this.leftStorageName, leftPercent);
     },
-    handleNodeClick() {},
+    handleNodeClick(node, data) {
+      this.reportCompent = node.component;
+    },
   },
 };
 </script>
---
+++ archive-ui/src/views/record/report/otherFileStatistic.vue
@@ -0,0 +1,114 @@
+<template>
+  <DataLayout>
+    <template v-slot:header style="height: auto">
+      <el-form :inline="true">
+        <el-form-item>
+          <el-date-picker
+            v-model="yearS"
+            type="year"
+            placeholder="选择年"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleReport()">{{
+            $t("application.SearchData")
+          }}</el-button>
+        </el-form-item>
+      </el-form>
+    </template>
+    <template v-slot:main="{ layout }">
+      <el-table
+        ref="mainTable"
+        :data="tables.mainTable.data"
+        border
+        stripe
+        size="mini"
+        v-loading="loading"
+        :height="layout.height"
+      >
+        <el-table-column type="index" width="30" fixed></el-table-column>
+        <el-table-column
+          v-for="item in tables.mainTable.columns"
+          :key="item.prop"
+          v-bind="item"
+        ></el-table-column>
+      </el-table>
+    </template>
+  </DataLayout>
+</template>
+<script>
+import DataLayout from "@/components/ecm-data-layout";
+export default {
+  name: "otherFileFlow",
+  data() {
+    return {
+      yearS: "",
+      loading: false,
+      tables: {
+        mainTable: {
+          data: [],
+          columns: [
+            {
+              prop: "fileType",
+              label: "文件类型",
+              fixed: true,
+              width: 110,
+            },
+            {
+              prop: "quarterOne",
+              label: "第一季度",
+              fixed: true,
+              width: 110,
+            },
+            {
+              prop: "quarterTwo",
+              label: "第二季度",
+              fixed: true,
+              width: 110,
+            },
+            {
+              prop: "quarterThree",
+              label: "第三季度",
+              fixed: true,
+              width: 110,
+            },
+            {
+              prop: "quarterFour",
+              label: "第四季度",
+              fixed: true,
+              width: 110,
+            },
+          ],
+        },
+      },
+    };
+  },
+
+  components: {
+    DataLayout: DataLayout,
+  },
+
+  methods: {
+    handleReport() {
+      let _self = this;
+      _self.loading = true;
+
+      var m = new Map();
+      m.set("yearSelect", _self.yearS);
+
+      axios
+        .post("/dms/record/otherFileStatistic", JSON.stringify(m))
+        .then(function (response) {
+          _self.tables.mainTable.data = response.data.data;
+          console.log(_self.tables.mainTable.data);
+          _self.loading = false;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
