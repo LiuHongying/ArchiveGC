bbb75616a6df100c856795f4df57e94c39eaa923
includeBD <1658209874@qq.com>
2021-01-28 18:59:07 +0800

    报表

--- archive-ui/src/views/record/report/index.js
+++ archive-ui/src/views/record/report/index.js
@@ -4,5 +4,10 @@ export {default as srWorkFlow} from './srWorkStatistic'
 export {default as otherFileFlow} from './otherFileStatistic'
 export {default as designWorkFlow} from './designWorkStatistic'
 export {default as commercialWorkFlow} from './commercialWorkStatistic'
+export {default as fileRecWorkFlow} from './fileRecWorkStatistic'
+export {default as fileUpdateWorkFlow} from './fileUpdateStatistic'
+export {default as fileRemoveWorkFlow} from './fileRemoveStatistic'
+export {default as otherFileUseWorkFlow} from './otherFileUseStatistic'
+
 
 
